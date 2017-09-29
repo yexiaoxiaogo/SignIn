6e38376d00aa082b54cb9b7447723c9d858ba0ca
yexiaoxiaogo <841079936@qq.com>
2017-09-29 22:34:21 +0800

    依赖d版本号修改。添加错误页面定制。templates的html文件格式文件

--- SingIn/src/main/java/io/github/yexiaoxiaogo/signin/controller/UserController.java
+++ SingIn/src/main/java/io/github/yexiaoxiaogo/signin/controller/UserController.java
@@ -1,11 +1,15 @@
 package io.github.yexiaoxiaogo.signin.controller;
 
+import javax.imageio.spi.RegisterableService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.yexiaoxiaogo.signin.domain.User;
 import io.github.yexiaoxiaogo.signin.service.UserService;
+import junit.framework.Test;
 
 @Controller
 public class UserController {
@@ -17,6 +21,28 @@ public class UserController {
 	public String index(Model model){
 
 			return "index";
+		
+	}
+//	@RequestMapping("/error")
+//	public String error(Model model){
+//
+//			return "error";
+//		
+//	}
+	
+	@RequestMapping("/test")
+	public String Test() {
+		return "test";
+	}
+	
+	//zhuce
+	@RequestMapping("/register")
+	public String register(){
+		//创建User对象
+		User user = new User();
+		//插入数据库
+		return null;
+		 
 		
 	}
 
