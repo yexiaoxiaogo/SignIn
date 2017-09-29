72ffdf033283e4bf2522e4da93aeda2e0d29942a
yexiaoxiaogo <841079936@qq.com>
2017-09-29 21:12:04 +0800

    前端index页面，可以登录or注册

--- SingIn/src/main/java/io/github/yexiaoxiaogo/signin/controller/UserController.java
+++ SingIn/src/main/java/io/github/yexiaoxiaogo/signin/controller/UserController.java
@@ -2,9 +2,9 @@ package io.github.yexiaoxiaogo.signin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import io.github.yexiaoxiaogo.signin.domain.User;
 import io.github.yexiaoxiaogo.signin.service.UserService;
 
 @Controller
@@ -14,14 +14,10 @@ public class UserController {
 	private UserService userService;
 	
 	@RequestMapping("/index")
-	public String index(String user,String password){
-		User user2 = userService.Login(user, password);
-		if(user2!=null){
-			return "success";
-		}
-		else{
-			return "fail";
-		}
+	public String index(Model model){
+
+			return "index";
+		
 	}
 
 
