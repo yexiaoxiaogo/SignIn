8f3edcc63ef4478b909f71d782a5702e94963eae
yexiaoxiaogo <841079936@qq.com>
2017-09-29 20:38:09 +0800

    注册业务

--- SingIn/src/main/java/io/github/yexiaoxiaogo/signin/service/UserService.java
+++ SingIn/src/main/java/io/github/yexiaoxiaogo/signin/service/UserService.java
@@ -5,4 +5,6 @@ import io.github.yexiaoxiaogo.signin.domain.User;
 public interface UserService {
 //业务层接口通过用户名user和用户密码password实现用户登录
 	public User Login(String user,String password);
+	//注册
+	public User Register(String user,String password);
 }
--- SingIn/src/main/java/io/github/yexiaoxiaogo/signin/service/impl/UserServiceImpl.java
+++ SingIn/src/main/java/io/github/yexiaoxiaogo/signin/service/impl/UserServiceImpl.java
@@ -9,11 +9,17 @@ import io.github.yexiaoxiaogo.signin.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
 	@Autowired
 	private UserDao userDao;
 	
+	//实现登录
 	public User Login(String user,String password){
 		return userDao.checkUser(user, password);
+	}
+	//实现注册
+	public User Register(String user,String  password){
+		return userDao.insertUser(user, password);
 	}
 
 }
